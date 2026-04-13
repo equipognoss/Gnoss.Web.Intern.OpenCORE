e94b7d6e889f830511c3536503b5f0e98d6b1ae8
Javier-RSP <javierruiz@gnoss.com>
2026-04-13 12:00:11 +0200

    Security Improvement

--- Gnoss.Web.Intern/Program.cs
+++ Gnoss.Web.Intern/Program.cs
@@ -1,6 +1,8 @@
+using Es.Riam.Gnoss.Util.General;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 using System;
 
 namespace Gnoss.Web.Intern
@@ -9,7 +11,19 @@ namespace Gnoss.Web.Intern
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            LoggingService.ConfigurarBasicStartupSerilog().CreateBootstrapLogger();
+            try
+            {
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Error fatal durante el arranque");
+            }
+            finally
+            {
+                Log.CloseAndFlush(); // asegura que se escriben todos los logs pendientes
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -21,6 +35,7 @@ namespace Gnoss.Web.Intern
                     config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
                     config.AddJsonFile($"appsettings.{hostContext.HostingEnvironment.EnvironmentName}.json", optional: true, reloadOnChange: true);
                 })
+                .UseSerilog((context, services, configuration) => LoggingService.ConfigurarSerilog(context.Configuration, services, configuration))
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
